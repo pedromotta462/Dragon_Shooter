9a5617b36a0cd0896499683545635874b35acd63
avulusion <joaogabriel.1c2015@gmail.com>
2017-08-10 12:56:01 -0300

    vai saber
    asfgasfasf

--- Assets/Scripts/Reset.cs
+++ Assets/Scripts/Reset.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class Reset : MonoBehaviour {
-	public GameObject txt;
+	
+	public List<GameObject> objs;
+	 
 	// Use this for initialization
 	void Start () {
-		txt.SetActive (false);
+		objs[0].SetActive (false);
+		objs[1].SetActive (false);
 		StartCoroutine (gg ());
 	}
 	
-	public void click(){
+	public void click2(){
+		objs[1].SetActive (false);
+		objs[2].SetActive (true);
+		objs[3].SetActive (true);
+		objs[4].SetActive (true);
 		PlayerPrefs.SetInt ("Netuno", 0);
 		PlayerPrefs.SetInt ("Marte", 0);
 		PlayerPrefs.SetInt ("Jupiter", 0);
@@ -21,12 +28,29 @@ public class Reset : MonoBehaviour {
 		PlayerPrefs.SetInt ("Mercurio", 0);
 		PlayerPrefs.SetInt ("Up", 0);
 		PlayerPrefs.SetFloat ("Speed", 0);
-		txt.SetActive (true);
+		objs[0].SetActive (true);
 	}
+	public void click1()
+	{
+		objs[1].SetActive (true);
+		objs[2].SetActive (false);
+		objs[3].SetActive (false);
+		objs[4].SetActive (false);
+
+	}
+	public void closar()
+	{
+		objs[1].SetActive (false);
+		objs[2].SetActive (true);
+		objs[3].SetActive (true);
+		objs[4].SetActive (true);
+
+	}
+
 	IEnumerator gg(){
 		while (true) {
 			yield return new WaitForSeconds (3);
-			txt.SetActive (false);
+			objs[0].SetActive (false);
 		}
 	}
 }
