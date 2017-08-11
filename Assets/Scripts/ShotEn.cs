09196215a29e1b995039a46f260f552c872298d4
avulusion <joaogabriel.1c2015@gmail.com>
2017-08-11 12:57:28 -0300

    vasfga
    asas

--- Assets/Scripts/ShotEn.cs
+++ Assets/Scripts/ShotEn.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class ShotEn : MonoBehaviour {
 	public Rigidbody shot;
-	public Transform world;
 	public bool boss;
 	PM dificuldade;
-	GameObject player;
+	GameObject player,world;
 	// Use this for initialization
 	void Start () {
+		world=GameObject.FindWithTag("Earth");
 		player = GameObject.FindWithTag ("Player");
 		dificuldade = player.GetComponent<PM> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		if (Time.timeScale==1) {
 			if (boss == false) {
 				shot.MovePosition (shot.position + transform.forward * dificuldade.vShotIni);
-				transform.LookAt (world);
+				transform.LookAt (world.transform);
 			}
 			shot.MovePosition (shot.position + transform.forward * dificuldade.vShotIni);
 
