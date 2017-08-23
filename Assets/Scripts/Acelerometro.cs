c1f82ccae2377209b639c393376a29ee8bdb9894
pedromotta462 <pedromotta1d2015@gmail.com>
2017-08-23 12:51:39 -0300

    56
    +84

---
+++ Assets/Scripts/Acelerometro.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acelerometro : MonoBehaviour {
+	//Rigidbody rb;
+	Camera cam;
+	// Use this for initialization
+	void Start () {
+		//rb = GetComponent<Rigidbody> ();
+		cam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var rot = transform.rotation;
+		Vector3 acele = Input.acceleration;
+
+		cam.transform.rotation = Quaternion.Euler(acele.x,acele.y,acele.z);
+	
+
+
+	}
+}
